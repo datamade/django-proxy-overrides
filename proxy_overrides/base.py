aa7a37b5539927e08986da91440ea5d5b58a639f
Forest Gregg <fgregg@uchicago.edu>
2018-11-27 14:55:23 -0600

    handle related name better

--- proxy_overrides/base.py
+++ proxy_overrides/base.py
@@ -20,22 +20,24 @@ def override_model_field(model, name, field):
                 field.remote_field.model.__name__
             ))
 
-        related_name = getattr(field.remote_field, 'related_name')
-        if related_name:
-            related_model = getattr(field.remote_field.model, related_name).rel.model
-            if related_model._meta.proxy:
-                raise TypeError('There is already a proxy model {!r} related to {!r} using {!r}'.format(
-                    related_model.__name__,
-                    field.remote_field.model.__name__,
-                    related_name
-                ))
+        related_name = field.remote_field.related_name
+        if not related_name:
+            related_name = original_field.remote_field.get_accessor_name()
+        related_model = getattr(field.remote_field.model, related_name).rel.model
 
-            model.add_to_class(name, field)
+        if related_model._meta.proxy:
+            raise TypeError('There is already a proxy model {!r} related to {!r} using {!r}'.format(
+                related_model.__name__,
+                field.remote_field.model.__name__,
+                related_name
+            ))
 
-            field.remote_field.model.add_to_class(
-                related_name,
-                ReverseManyToOneDescriptor(field.remote_field)
-            )
+        model.add_to_class(name, field)
+
+        field.remote_field.model.add_to_class(
+            related_name,
+            ReverseManyToOneDescriptor(field.remote_field)
+        )
 
 
 class ProxyField(object):
